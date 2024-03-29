ca72601abdcdf3ed55dd0e775d7bf5a36edf242b
Shubham-Goenka <shubham.17101997.goenka@gmail.com>
2024-03-30 00:06:48 +0530

    Removing columns

--- PortfolioManagement/stocksManagement.py
+++ PortfolioManagement/stocksManagement.py
@@ -6,11 +6,16 @@ class StocksManagement(object):
     def __init__(self):
         self.imported_data = None
         self.formulated_data = None
+        self.data = None
         pass
 
     def load_data(self, filename):
         self.imported_data = pd.read_csv(filename)
+        self.data = self.imported_data
 
     def formulate_data(self):
         pass
+
+    def drop_columns(self, columns):
+        self.data.drop(columns=columns, inplace=True)
 

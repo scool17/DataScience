4363edd7848b39eca554c286647ce637e840c189
Shubham-Goenka <shubham.17101997.goenka@gmail.com>
2024-04-07 00:07:17 +0530

    Calculating Gradients of Linear Regression

--- MachineLearningAlgos/LinearRegression.py
+++ MachineLearningAlgos/LinearRegression.py
@@ -1,5 +1,24 @@
 from MachineLearningAlgos.Regression import CustomRegression
 
+import numpy as np
 
 class CustomLinearRegression(CustomRegression):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update_wights(self, learning_rate, weight0, weight1):
+        weight0 -= learning_rate * weight0
+        weight1 -= learning_rate * weight1
+
+    def predict(self, x, w, w0):
+        y_hat = x*w + w0
+
+    def squared_loss_error(self, y_hat, y):
+        error = np.mean((y_hat - y) ** 2)
+
+    def gradient(self, y, y_hat, x):
+        weight0 = -2 * (y - y_hat)
+        weight1 = -2 * (y - y_hat) * x
+
